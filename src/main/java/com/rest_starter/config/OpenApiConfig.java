90748bbd6c16b0bf24e7b8c9d32ba2f53dcebe72
huaytadev <contactohuayta@gmail.com>
2026-07-04 19:10:39 -0300

    Delete the static server I set up to test the project locally

--- src/main/java/com/rest_starter/config/OpenApiConfig.java
+++ src/main/java/com/rest_starter/config/OpenApiConfig.java
@@ -1,22 +1,19 @@
 package com.rest_starter.config;
 
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
     @Bean
     public OpenAPI restStarterOpenApi() {
-        Server localServer = new Server()
-                .url("http://localhost:8080")
-                .description("Local development server");
+//        Server localServer = new Server()
+//                .url("http://localhost:8080")
+//                .description("Local development server");
 
         Contact contact = new Contact()
                 .name("REST Starter API")
@@ -29,7 +26,7 @@ public class OpenApiConfig {
                 .contact(contact);
 
         return new OpenAPI()
-                .info(info)
-                .servers(List.of(localServer));
+                .info(info);
+//                .servers(List.of(localServer));
     }
 }
